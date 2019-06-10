ef0424dc5cb9d443b8e55552da212de08628c954
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2019-06-10 12:25:18 -0600

    cache is close - needs bug checking

--- services/lruCache.js
+++ services/lruCache.js
@@ -20,11 +20,18 @@ If it is not a hit - push the new, timestamp updated
 
 */
 lruCache(-10,10);
-lruCache(-10,11);
-// setTimeout('', 5000);
+lruCache(-99,99);
 lruCache(-10,10);
-// setTimeout('', 5000);
 lruCache(-10,10);
+lruCache(1,1);
+lruCache(1,2);
+lruCache(1,3);
+lruCache(1,4);
+lruCache(1,2);
+lruCache(1,5);
+lruCache(1,6);
+lruCache(1,7);
+lruCache(1,8);
 
 function lruCache(latitude, longitude) {
     this.get = function(latitude,longitude) {
@@ -32,16 +39,24 @@ function lruCache(latitude, longitude) {
         console.log(`Changed to key: ${key}`);
         if (theCacheItself.has(key)) {
             console.log(`Cool a cache hit!`);
-            lruCacheHitNotFull++;
-            lruCacheHitFull++;
+            if(this.sizeCheck()) {
+                lruCacheHitFull++;
+            }
+            else {
+                lruCacheHitNotFull++;
+            }
             this.pop(key);
             this.push(key);
             return 0;
         }
         else {
             console.log(`Cache miss - adding key ${key}`);
-            lruCacheMissFull++;
-            lruCacheMissNotFull++;
+            if(this.sizeCheck()) {
+                lruCacheMissFull++;
+            }
+            else {
+                lruCacheMissNotFull++;
+            }
             this.push(key);
             return 0;
         }
@@ -56,7 +71,25 @@ function lruCache(latitude, longitude) {
         theCacheItself.delete(key);
         return 0;
     },
-
+    this.sizeCheck = function() {
+        let sizeCheck; // True = over size ---- False = under size
+        let currentSize = theCacheItself.size;
+        // We are including AT SIZE in the TRUE portion as another push at this point would put us over size
+        console.log(`Current Size = ${currentSize} - MAX: ${LRUCACHESIZE}`)
+        if (currentSize >= LRUCACHESIZE) {
+            sizeCheck = true
+            let firstItemKey = theCacheItself.keys().next().value // list all keys, take 'next' or first value... which is a key
+            console.log(`removing first item... ${firstItemKey}`)
+            this.pop(firstItemKey)
+        }
+        else if (currentSize < LRUCACHESIZE) {
+            sizeCheck = false
+        }
+        else {
+            let error = 'somehow we have an issue with our size'
+        }
+        return sizeCheck;
+    }
     this.clearCache = function() {
         theCacheItself.clear();
         console.log(`Should be empty: ${theCacheItself}`);
