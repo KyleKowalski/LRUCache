514be2b755112382a3e763c3952cfb7e1bdae73a
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2019-06-10 10:20:24 -0600

    slight tweak to API routes, cleanup and adjust wording - now to build the cache itself

--- routes/lruCacheRoutes.js
+++ routes/lruCacheRoutes.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('basic instructions on how to use API here... obvious placeholder is obvious...');
+  res.status(200).send('basic instructions on how to use API here... obvious placeholder is obvious...');
 });
 
 router.get('/stats', function(req, res, next) {
-  res.send('basic instructions on how to use API here... obvious placeholder is obvious...');
+  res.status(200).send('here are our stats for the cache...');
 });
 
 router.get('/marsCoords/latitude/:latitude/longitude/:longitude', function (req, res) {
@@ -17,7 +17,7 @@ router.get('/marsCoords/latitude/:latitude/longitude/:longitude', function (req,
 
   // validate latitude
   if (latitude < -90) {
-    error.latitude = `Latitude is below (-90 to 90) at ${latitude}`;
+    error.latitude = `Latitude is below range (-90 to 90) at ${latitude}`;
   }
   else if (latitude > 90) {
     error.latitude = `Latitude is above range (-90 to 90) at ${latitude}`;
@@ -28,7 +28,7 @@ router.get('/marsCoords/latitude/:latitude/longitude/:longitude', function (req,
 
   // validate longitude
   if (longitude < -180) {
-    error.longitude = `Longitude is below (-180 to 180) at ${longitude}`;
+    error.longitude = `Longitude is below range (-180 to 180) at ${longitude}`;
   }
   else if (longitude > 180) {
     error.longitude = `Longitude is above range (-180 to 180) at ${longitude}`;
