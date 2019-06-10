ca61a5913d698df5028ee89aeaffb29e65976ddd
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2019-06-10 10:08:52 -0600

    router is looking solid enough, still need to implement the entire cache

---
+++ routes/lruCacheRoutes.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('basic instructions on how to use API here... obvious placeholder is obvious...');
+});
+
+router.get('/stats', function(req, res, next) {
+  res.send('basic instructions on how to use API here... obvious placeholder is obvious...');
+});
+
+router.get('/marsCoords/latitude/:latitude/longitude/:longitude', function (req, res) {
+  let latitude = parseInt(req.params.latitude);
+  let longitude = parseInt(req.params.longitude);
+  let error = {};
+
+  // validate latitude
+  if (latitude < -90) {
+    error.latitude = `Latitude is below (-90 to 90) at ${latitude}`;
+  }
+  else if (latitude > 90) {
+    error.latitude = `Latitude is above range (-90 to 90) at ${latitude}`;
+  }
+  else if (isNaN(latitude)) {
+    error.latitude = `Latitude is not a number`;
+  }
+
+  // validate longitude
+  if (longitude < -180) {
+    error.longitude = `Longitude is below (-180 to 180) at ${longitude}`;
+  }
+  else if (longitude > 180) {
+    error.longitude = `Longitude is above range (-180 to 180) at ${longitude}`;
+  }
+  else if (isNaN(longitude)) {
+    error.longitude = `Longitude is not a number`;
+  }
+
+  // if both validate, start doing what we're here for (get the url)
+  if (!(error.latitude || error.longitude)) {
+    // call for the item from the cache
+    // let result = lruCache(latitude, longitude);
+    let result = `Received Lat/Long of: ${latitude}/${longitude}`
+    res.status(200).json(result);
+  }
+  else {
+    res.status(400).json(error);
+  }
+});
+
+// Delete listing
+router.delete('/', function(req, res, next) {
+  res.status(200).send('annnd we are nuking the cache, cool');
+});
+
+module.exports = router;
---
+++ services/lruCache.js
@@ -0,0 +1,1 @@
+console.log('utterly simple LRU cache load')
